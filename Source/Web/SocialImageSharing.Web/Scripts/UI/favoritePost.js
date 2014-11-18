50e2e775c26a177fd717e3ebda060a0ff1d54566
Derpessor96 <ivan.tonchev96@gmail.com>
2014-11-18 04:34:46 +0200

    Removed some old javascript.

---
+++ Source/Web/SocialImageSharing.Web/Scripts/UI/favoritePost.js
@@ -0,0 +1,9 @@
+﻿function favorite() {
+	$("#favorite-button").hide();
+	$("#unfavorite-button").show();
+}
+
+function unfavorite() {
+	$("#favorite-button").show();
+	$("#unfavorite-button").hide();
+}
